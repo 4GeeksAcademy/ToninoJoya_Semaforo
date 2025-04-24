2a19644fa2531c59ae47506c761c3521a16fa38f
ToninoJoya <antoniocaprab@gmail.com>
2025-04-24 12:58:14 +0000

    Semaforo Refactorizado

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -6,43 +6,76 @@ import { useState } from "react";
 
 export const Semaforo = () => {
 
-    const [color, setColor] = useState(null)
+    const [colorSeleccionado, setColor] = useState(null)
 
-
+    const coloresSemaforo = [ "rojo" , "amarillo" , "verde"]
 
 
     return (
-
-        <>
-            <div className="container d-flex justify-content-center">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="container d-flex justify-content-center">
-                            <div className="bg-dark palo">
-                            </div>
+        <div className="container d-flex justify-content-center">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="container d-flex justify-content-center">
+                        <div className="bg-dark palo">
                         </div>
+                    </div>
+                    <div className=" bg-dark semaforo rounded-4 mt-0 d-flex flex-column justify-content-center align-items-center">
+                        {
+                            coloresSemaforo.map((color, index) => {
+                                console.log(coloresSemaforo)
+                                return (
+                                    <div
+                                        key={index}
+                                        className={`mt-2 rounded-circle ${color} ${colorSeleccionado === color ? "glow" : ""}`}
+                                        onClick={() => setColor(color)}>
 
-                        <div className=" bg-dark semaforo rounded-4 mt-0 d-flex flex-column justify-content-center align-items-center">
-                            <div
-                                className={`rojo btn btn-danger mt-2 rounded-circle ${color === "rojo" ? "glow" : ""}`}
-                                onClick={() => setColor("rojo")}>
-
-                            </div>
-                            <div className={`amarillo btn btn-warning mt-2 rounded-circle ${color === "amarillo" ? "glow" : ""}`}
-                                onClick={() => setColor("amarillo")}> 
-
-                            </div>
-                            <div className={`verde btn btn-success mt-2 mb-2 rounded-circle ${color === "verde" ? "glow" : ""}`}
-                                onClick={() => setColor("verde")}>
-
-                            </div>
-                        </div>
-
+                                    </div>
+                                )
+                            })}
                     </div>
                 </div>
-            </div >
-        </>
+            </div>
+        </div>
     );
-
-
 };
+
+
+
+
+
+
+
+
+
+// return (
+
+//     <>
+//         <div className="container d-flex justify-content-center">
+//             <div className="row">
+//                 <div className="col-lg-12">
+//                     <div className="container d-flex justify-content-center">
+//                         <div className="bg-dark palo">
+//                         </div>
+//                     </div>
+
+//                     <div className=" bg-dark semaforo rounded-4 mt-0 d-flex flex-column justify-content-center align-items-center">
+//                         <div
+//                             className={`rojo btn btn-danger mt-2 rounded-circle ${color === "rojo" ? "glow" : ""}`}
+//                             onClick={() => setColor("rojo")}>
+
+//                         </div>
+//                         <div className={`amarillo colorAmarillo mt-2 rounded-circle ${color === "amarillo" ? "glow" : ""}`}
+//                             onClick={() => setColor("amarillo")}>
+
+//                         </div>
+//                         <div className={`verde btn btn-success mt-2 mb-2 rounded-circle ${color === "verde" ? "glow" : ""}`}
+//                             onClick={() => setColor("verde")}>
+
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div >
+//     </>
+// );
